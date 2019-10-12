ed32c08a16b087202a3b504f559698301826a304
Masataka Hisasue <masataka.hisasue@optim.co.jp>
2019-10-12 23:37:11 +0900

    style: move directory

--- int.go
+++ int.go
@@ -1,8 +1,6 @@
-package intm
+package perseus
 
-import ()
-
-func In(i int, list []int) bool {
+func InInt(i int, list []int) bool {
 	for _, b := range list {
 		if b == i {
 			return true
@@ -11,7 +9,7 @@ func In(i int, list []int) bool {
 	return false
 }
 
-func Index(s int, list []int) int {
+func IndexInt(s int, list []int) int {
 	for i, b := range list {
 		if b == s {
 			return i
@@ -20,39 +18,43 @@ func Index(s int, list []int) int {
 	return -1
 }
 
-func Shift(slice []int) (int, []int) {
+func ShiftInt(slice []int) (int, []int) {
 	x, s := slice[0], slice[1:]
 	return x, s
 }
 
-func Unshift(sep int, i []int) []int {
+func UnshiftInt(sep int, i []int) []int {
 	return append([]int{sep}, i...)
 }
 
-func Delete(slice []int, sep int) []int {
+func DeleteInt(slice []int, sep int) []int {
 	return append(slice[:sep], slice[sep+1:]...)
 }
 
-func Cut(slice []int, i, j int) []int {
+func CutInt(slice []int, i, j int) []int {
 	return append(slice[:i], slice[j:]...)
 }
 
-func Insert(slice []int, element, position int) []int {
+func InsertInt(slice []int, element, position int) []int {
 	return append(slice[:position], append([]int{element}, slice[position:]...)...)
 }
 
-func Pop(slice []int) (int, []int) {
+func InsertVectorInt(origin, insert []int, position int) []int {
+	return append(origin[:position], append(insert, origin[position:]...)...)
+}
+
+func PopInt(slice []int) (int, []int) {
 	x, s := slice[len(slice)-1], slice[:len(slice)-1]
 	return x, s
 }
 
-func Reversed(slice []int) []int {
+func ReversedInt(slice []int) []int {
 	for left, right := 0, len(slice)-1; left < right; left, right = left+1, right-1 {
 		slice[left], slice[right] = slice[right], slice[left]
 	}
 	return slice
 }
 
-func Extend(A, B []int) []int {
+func ExtendInt(A, B []int) []int {
 	return append(A, B...)
 }
