94e0f153e08148eab1345da06fc44dad74dfe20d
Masataka Hisasue <masataka.hisasue@gmail.com>
2025-03-02 19:59:21 +0900

    Feat use generics

--- int.go
+++ int.go
@@ -1,87 +1,57 @@
 package perseus
 
-// InInt returns whether i is in list
-func InInt(i int, list []int) bool {
-	for _, b := range list {
-		if b == i {
-			return true
-		}
-	}
-	return false
-}
+import "golang.org/x/exp/constraints"
 
-// IndexInt returns the position of s in list. If s is not found, return -1.
-func IndexInt(s int, list []int) int {
-	for i, b := range list {
-		if b == s {
-			return i
-		}
-	}
-	return -1
-}
-
-// ShiftInt returns the first element of slice and other element's slice.
-func ShiftInt(slice []int) (int, []int) {
+// Shift returns the first element of slice and other element's slice.
+func Shift[T any](slice []T) (T, []T) {
 	return slice[0], slice[1:]
 }
 
-// UnshiftInt add an element to the beginning of a slice.
-func UnshiftInt(sep int, i []int) []int {
-	return append([]int{sep}, i...)
+// Prepend add an element to the beginning of a slice.
+func Prepend[T any](sep T, i []T) []T {
+	return append([]T{sep}, i...)
 }
 
-// DeleteInt Delete specified element from slice
-func DeleteInt(slice []int, sep int) []int {
+// Remove Remove specified element from slice
+func Remove[K any, V constraints.Integer](slice []K, sep V) []K {
 	return append(slice[:sep], slice[sep+1:]...)
 }
 
-// CutInt Delete from i to j from the slice
-func CutInt(slice []int, i, j int) []int {
+// Cut Delete from i to j from the slice
+func Cut[K any, V constraints.Integer](slice []K, i, j V) []K {
 	return append(slice[:i], slice[j:]...)
 }
 
-// InsertInt Insert element to specified position
-func InsertInt(slice []int, element, position int) []int {
-	return append(slice[:position], append([]int{element}, slice[position:]...)...)
+// Insert Insert element to specified position
+func Insert[K any, V constraints.Integer](slice []K, element K, position V) []K {
+	return append(slice[:position], append([]K{element}, slice[position:]...)...)
 }
 
-// InsertVectorInt Insert slice to specified position
-func InsertVectorInt(origin, insert []int, position int) []int {
+// InsertVector Insert slice to specified position
+func InsertVector[K any, V constraints.Integer](origin, insert []K, position V) []K {
 	return append(origin[:position], append(insert, origin[position:]...)...)
 }
 
-// PopInt returns the last element of slice and other element's slice.
-func PopInt(slice []int) (int, []int) {
+// Pop returns the last element of slice and other element's slice.
+func Pop[T any](slice []T) (T, []T) {
 	return slice[len(slice)-1], slice[:len(slice)-1]
 }
 
-// ReversedInt returns reversed slice
-func ReversedInt(slice []int) []int {
-	for left, right := 0, len(slice)-1; left < right; left, right = left+1, right-1 {
-		slice[left], slice[right] = slice[right], slice[left]
-	}
-	return slice
-}
-
-// ExtendInt connect slices together
-func ExtendInt(A, B []int) []int {
+// Extend connect slices together
+func Extend[T any](A, B []T) []T {
 	return append(A, B...)
 }
 
-func sumInt(values ...int) int64 {
-	var sum int64
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
+func Sum[T Number](values []T) T {
+	var sum T
+
 	for _, v := range values {
-		sum += int64(v)
+		sum += v
 	}
+
 	return sum
-}
-
-// SumInt calculate summaries of arguments
-func SumInt(values ...int) int {
-	return int(sumInt(values...))
-}
-
-// SumIntToInt64 calculate summaries of arguments
-func SumIntToInt64(values ...int) int64 {
-	return sumInt(values...)
 }
--- perseus_test.go
+++ perseus_test.go
@@ -2,103 +2,106 @@ package perseus_test
 
 import (
 	"fmt"
+	"testing"
 
 	"github.com/HMasataka/perseus"
+	"github.com/stretchr/testify/assert"
 )
 
-func ExampleInInt() {
+func TestShift(t *testing.T) {
 	v := []int{1, 2, 3, 4, 5}
 
-	b := perseus.InInt(1, v)
-	fmt.Printf("In : %t\n", b)
-	// Output:
-	// In : true
-}
+	x, s := perseus.Shift(v)
+	assert.Equal(t, []int{2, 3, 4, 5}, s)
 
-func ExampleIndexInt() {
-	v := []int{1, 2, 3, 4, 5}
-
-	i := perseus.IndexInt(3, v)
-	fmt.Printf("Index : %d\n", i)
-	// Output:
-	// Index : 2
-}
-
-func ExampleShiftInt() {
-	v := []int{1, 2, 3, 4, 5}
-
-	x, s := perseus.ShiftInt(v)
 	fmt.Printf("Shift : %d, %v\n", x, s)
 	// Output:
 	// Shift : 1, [2 3 4 5]
 }
 
-func ExampleUnshiftInt() {
+func TestPrepend(t *testing.T) {
 	v := []int{1, 2, 3, 4, 5}
 
-	u := perseus.UnshiftInt(1, v)
-	fmt.Printf("Unshift : %v\n", u)
+	u := perseus.Prepend(1, v)
+	assert.Equal(t, []int{1, 1, 2, 3, 4, 5}, u)
+
+	fmt.Printf("Prepend : %v\n", u)
 	// Output:
-	// Unshift : [1 1 2 3 4 5]
+	// Prepend : [1 1 2 3 4 5]
 }
 
-func ExampleDeleteInt() {
+func TestRemove(t *testing.T) {
 	v := []int{1, 2, 3, 4, 5}
 
-	d := perseus.DeleteInt(v, 3)
-	fmt.Printf("Delete : %v\n", d)
+	d := perseus.Remove(v, 3)
+	assert.Equal(t, []int{1, 2, 3, 5}, d)
+
+	fmt.Printf("Remove : %v\n", d)
 	// Output:
-	// Delete : [1 2 3 5]
+	// Remove : [1 2 3 5]
 }
-func ExampleCutInt() {
+
+func TestCut(t *testing.T) {
 	v := []int{1, 2, 3, 4, 5}
 
-	c := perseus.CutInt(v, 2, 3)
+	c := perseus.Cut(v, 2, 3)
+	assert.Equal(t, []int{1, 2, 4, 5}, c)
+
 	fmt.Printf("Cut : %v\n", c)
 	// Output:
 	// Cut : [1 2 4 5]
 
 }
-func ExampleInsertInt() {
+
+func TestInsert(t *testing.T) {
 	v := []int{1, 2, 3, 4, 5}
 
-	insert := perseus.InsertInt(v, 10, 3)
+	insert := perseus.Insert(v, 10, 3)
+	assert.Equal(t, []int{1, 2, 3, 10, 4, 5}, insert)
+
 	fmt.Printf("Insert : %v\n", insert)
 	// Output:
 	// Insert : [1 2 3 10 4 5]
 }
 
-func ExampleInsertVectorInt() {
+func TestInsertVector(t *testing.T) {
 	v := []int{1, 2, 3, 4, 5}
 
-	insertV := perseus.InsertVectorInt(v, []int{15, 14, 13, 12}, 3)
-	fmt.Printf("InsertV : %v\n", insertV)
+	insertV := perseus.InsertVector(v, []int{15, 14, 13, 12}, 3)
+	assert.Equal(t, []int{1, 2, 3, 15, 14, 13, 12, 4, 5}, insertV)
+
+	fmt.Printf("InsertVector : %v\n", insertV)
 	// Output:
-	// InsertV : [1 2 3 15 14 13 12 4 5]
+	// InsertVector : [1 2 3 15 14 13 12 4 5]
 }
 
-func ExamplePopInt() {
+func TestPop(t *testing.T) {
 	v := []int{1, 2, 3, 4, 5}
 
-	p, vv := perseus.PopInt(v)
+	p, vv := perseus.Pop(v)
+	assert.Equal(t, []int{1, 2, 3, 4}, vv)
+
 	fmt.Printf("Pop : %d, %v\n", p, vv)
 	// Output:
 	// Pop : 5, [1 2 3 4]
 }
 
-func ExampleReversedInt() {
+func TestExtend(t *testing.T) {
 	v := []int{1, 2, 3, 4, 5}
+	e := perseus.Extend(v, []int{11, 12, 13, 14, 15})
+	assert.Equal(t, []int{1, 2, 3, 4, 5, 11, 12, 13, 14, 15}, e)
 
-	r := perseus.ReversedInt(v)
-	fmt.Printf("Reversed : %v\n", r)
-	// Output:
-	// Reversed : [5 4 3 2 1]
-}
-
-func ExampleExtendInt() {
-	v := []int{1, 2, 3, 4, 5}
-	e := perseus.ExtendInt(v, []int{11, 12, 13, 14, 15})
 	fmt.Printf("Extend : %v\n", e)
 	// Output:
 	// Extend : [1 2 3 4 5 11 12 13 14 15]
+}
+
+func TestSum(t *testing.T) {
+	v := []int{1, 2, 3, 4, 5}
+	e := perseus.Sum(v)
+	assert.Equal(t, 15, e)
+
+	fmt.Printf("Sum : %v\n", e)
+	// Output:
+	// Extend :[15
 }
